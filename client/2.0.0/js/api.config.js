84aa85716e08798734f1d2d01d31f907f3e8e84e
PC-Axis <lorenzo.bruni@cso.ie>
2023-09-22 11:59:11 +0100

    cleanup

--- client/2.0.0/js/api.config.js
+++
@@ -1,8 +0,0 @@
-/*******************************************************************************
-API - Configuration 
-*******************************************************************************/
-
-// Preset Ajax calls to be ASynch by default
-$.ajaxPrefilter(function (options, originalOptions, jqXHR) {
-    options.async = originalOptions.async === undefined ? true : originalOptions.async;
-});
--- client/3.0.0/template/js/app.constant.js
+++
@@ -1,6 +0,0 @@
-/*******************************************************************************
-Application - Constant
-*******************************************************************************/
-
-// Sample of a Application Constant
-const C_APP_SOMETHING = 'Assign here the value of the constant';
